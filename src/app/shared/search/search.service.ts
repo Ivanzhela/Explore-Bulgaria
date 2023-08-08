500f12e986d7e476b47fcd57f72e97fb4a19f7af
Ivanzhela <ivanjelad@abv.bg>
2023-08-08 15:21:50 +0300

    update environments

--- src/app/shared/search/search.service.ts
+++ src/app/shared/search/search.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Place } from 'src/app/types/place';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SearchService {
+  baseUrl: string = environment.baseUrl;
+  
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getSearchDestination(title? : string) {
+  getSearchDestination(title?: string) {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const url = `${environment.baseUrl}/google/search`;
-    return this.http.post<Place>(url, { destination: title }, {headers})
+    return this.http.post<Place>(url, { destination: title }, { headers });
   }
 }
